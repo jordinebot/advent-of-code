0290d6fce80b2cf916bbc2c1e92dfd6102cfccc8
Jordi Nebot <jordi.nebot@king.com>
2020-12-01 21:08:54 +0100

    Add time calculation

--- 2020/day-1/main.py
+++ 2020/day-1/main.py
@@ -2,25 +2,31 @@
 Part 1: Find the two entries that sum to 2020 and then multiply those two numbers together
 Part 2: What is the product of the three entries that sum to 2020?
 '''
+import time
 
 f = open("input", "rt")
 data = list(f)
 
 def product_of_two():
-	for i in data:
-		for j in data:
-			if int(i) + int(j) == 2020:
-				return int(i) * int(j)
+	for x in data:
+		for y in data:
+			if int(x) + int(y) == 2020:
+				return int(x) * int(y)
 
 def product_of_three():
-	for i in data:
-		for j in data:
-			for k in data:
-				if int(i) + int(j) + int(k) == 2020:
-					return int(i) * int(j) * int(k)
+	for x in data:
+		for y in data:
+			for z in data:
+				if int(x) + int(y) + int(z) == 2020:
+					return int(x) * int(y) * int(z)
 
+t1 = time.time()
+part1 = product_of_two()
+t2 = time.time()
+print("Part 1: %d calculated in %fs" % (part1, t2 - t1))
 
+t1 = time.time()
+part2 = product_of_three()
+t2 = time.time()
+print("Part 2: %d calculated in %fs" % (part2, t2 - t1))
 
-print(product_of_two())
-
-print(product_of_three())
