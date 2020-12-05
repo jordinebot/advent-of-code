f1b213a724f466fbaa7ff1e33b27054e482bebad
Jordi Nebot <jordi.nebot@king.com>
2020-12-05 17:22:53 +0100

    Add 2020 solution for day-5, part-2

--- 2020/day-5/main.py
+++ 2020/day-5/main.py
@@ -1,22 +1,23 @@
 f = open("input", "rt")
 boarding_passes = f.read().strip().split("\n")
 
-def get_seat_id(boarding_pass):
-	row_code = boarding_pass[:7]
-	col_code = boarding_pass[7:]
-
+def get_seat(boarding_pass):
 	row = [i for i in range(128)]
-	for r in row_code:
+	for r in boarding_pass[:7]:
 		if r == "F": row = row[:len(row) // 2]
 		if r == "B": row = row[len(row) // 2:]
 
 	col = [i for i in range(8)]
-	for c in col_code:
+	for c in boarding_pass[7:]:
 		if c == "L": col = col[:len(col) // 2]
 		if c == "R": col = col[len(col) // 2:]
 
 	return row[0] * 8 + col[0]
 
 
-seat_ids = sorted([*map(get_seat_id, boarding_passes)])
-print("Part 1: The highest seat ID from the list of boarding passes is %d" % seat_ids[-1])
+seats = sorted([*map(get_seat, boarding_passes)])
+print("Part 1: The highest seat ID from the list of boarding passes is %d" % seats[-1])
+
+my_boarding_pass = [i for i in range(seats[0], seats[-1]) if not i in seats]
+print("Part 2: My boarding pass is %d" % my_boarding_pass[0])
+
