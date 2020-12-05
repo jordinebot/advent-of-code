74abb3ddc4b07bf0c7313ddc7b6089ffee0e2482
Jordi Nebot <jordi.nebot@king.com>
2020-12-05 17:48:20 +0100

    Optimize 2020 day-5 solution

--- 2020/day-5/main.py
+++ 2020/day-5/main.py
@@ -2,22 +2,15 @@ f = open("input", "rt")
 boarding_passes = f.read().strip().split("\n")
 
 def get_seat(boarding_pass):
-	row = [i for i in range(128)]
-	for r in boarding_pass[:7]:
-		if r == "F": row = row[:len(row) // 2]
-		if r == "B": row = row[len(row) // 2:]
-
-	col = [i for i in range(8)]
-	for c in boarding_pass[7:]:
-		if c == "L": col = col[:len(col) // 2]
-		if c == "R": col = col[len(col) // 2:]
-
-	return row[0] * 8 + col[0]
-
+	seat = [i for i in range(1024)]
+	for x in boarding_pass:
+		if x in ["F", "L"]: seat = seat[:len(seat) // 2]
+		if x in ["B", "R"]: seat = seat[len(seat) // 2:]
+	return seat[0]
 
 seats = sorted([*map(get_seat, boarding_passes)])
-print("Part 1: The highest seat ID from the list of boarding passes is %d" % seats[-1])
+print("Part 1: The highest seat from the list of boarding passes is %d" % seats[-1])
 
-my_boarding_pass = [i for i in range(seats[0], seats[-1]) if not i in seats]
-print("Part 2: My boarding pass is %d" % my_boarding_pass[0])
+my_seat = [i for i in range(seats[0], seats[-1]) if not i in seats]
+print("Part 2: My seat is %d" % my_seat[0])
 
