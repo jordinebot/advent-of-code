4136cccbea1dcc69d981e25e657e645720d1c9eb
Jordi Nebot <jordi.nebot@king.com>
2020-12-08 11:33:00 +0100

    Add solution for 2020 day-8, part-1

---
+++ 2020/day-8/main.py
@@ -0,0 +1,18 @@
+f = open("input", "rt")
+program = list(f.read().strip().split("\n"))
+
+i = 0
+acc = 0
+ran = []
+
+while not i in ran:
+	ran.append(i)
+	ins, val = program[i].split(" ")
+	if ins == "nop": i += 1
+	elif ins == "jmp": i += int(val)
+	elif ins == "acc":
+		i += 1
+		acc += int(val)
+
+print(acc)
+
