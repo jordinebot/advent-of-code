1b29f8b5f9d1e91ffa966b377ef85ef50a8cd59d
Jordi Nebot <jordi.nebot@king.com>
2020-12-08 16:15:45 +0100

    Improve 2020 day-8, part-1

--- 2020/day-8/main.py
+++ 2020/day-8/main.py
@@ -1,18 +1,31 @@
+import copy
+
 f = open("input", "rt")
-program = list(f.read().strip().split("\n"))
+program = [*map(lambda x: x.split(" "), list(f.read().strip().split("\n")))]
 
-i = 0
-acc = 0
-ran = []
+def exe(ins, val, i, acc):
+    if ins == "nop":
+        i += 1
+    elif ins == "jmp":
+        i += int(val)
+    elif ins == "acc":
+        i += 1
+        acc += int(val)
+    return (i, acc)
 
-while not i in ran:
-	ran.append(i)
-	ins, val = program[i].split(" ")
-	if ins == "nop": i += 1
-	elif ins == "jmp": i += int(val)
-	elif ins == "acc":
-		i += 1
-		acc += int(val)
 
+def run(program):
+    i = 0
+    acc = 0
+    ran = []
+    while not i in ran:
+        if i >= len(program):
+            return (True, acc)
+        else:
+            ran.append(i)
+            ins, val = program[i]
+            i, acc = exe(ins, val, i, acc)
+    return (False, acc)
+
+exit, acc = run(program)
 print("Part 1: The value of acc before jumping into the second infinite loop iteration is %d" % acc)
-
