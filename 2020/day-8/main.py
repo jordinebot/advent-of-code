e654018abbd28c0317d69cc4016449d32e7800df
Jordi Nebot <jordi.nebot@king.com>
2020-12-08 16:34:07 +0100

    Add solution for 2020 day-8, part-2

--- 2020/day-8/main.py
+++ 2020/day-8/main.py
@@ -2,6 +2,7 @@ import copy
 
 f = open("input", "rt")
 program = [*map(lambda x: x.split(" "), list(f.read().strip().split("\n")))]
+original = copy.deepcopy(program)
 
 def exe(ins, val, i, acc):
     if ins == "nop":
@@ -29,3 +30,28 @@ def run(program):
 
 exit, acc = run(program)
 print("Part 1: The value of acc before jumping into the second infinite loop iteration is %d" % acc)
+
+
+def find_next_fork(forked):
+    fork = copy.deepcopy(original)
+    for i in range(forked[-1] + 1, len(original)):
+        ins, val = fork[i]
+        if ins == "nop":
+            forked.append(i)
+            fork[i] = ["jmp", val]
+            return fork
+        elif ins == "jmp":
+            forked.append(i)
+            fork[i] = ["nop", val]
+            return fork
+
+
+forked = [0]
+exit, acc = run(program)
+while not exit:
+    program = find_next_fork(forked)
+    exit, acc = run(program)
+
+print("Part 2: The value of acc after fixing the program is %d" % acc)
+
+
