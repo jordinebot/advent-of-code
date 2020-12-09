6b41af9437b731ef0335d4d1f455450d4611f6f7
Jordi Nebot <jordi.nebot@king.com>
2020-12-09 15:34:32 +0100

    Fix algorithm for 2020 day-9, part-1

--- 2020/day-9/main.py
+++ 2020/day-9/main.py
@@ -5,21 +5,21 @@ data = [*map( lambda x: int(x), f.read().strip().split("\n"))]
 
 preamble = 25
 
-def check(i, data):
+def check(i):
 	if i < preamble:
 		return True
 	else:
 		n = data[i]
-		subset = data[:i]
-		for x in range(0, len(subset)):
-			for y in range(x + 1, len(subset)):
+		for x in range(i - preamble, i):
+			for y in range(i - preamble + 1, i):
 				if data[x] + data[y] == n:
 					return True
 		return False
 
 t0 = time.time()
 for i in range(preamble, len(data)):
-	if not check(i, data):
+	if not check(i):
 		t1 = time.time()
 		print("Part 1: The first number that is not the sum of two previous ones is %d (%fms)" % (data[i], (t1-t0)*1000))
 		break
+
