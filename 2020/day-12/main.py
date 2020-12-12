c109dd7905d4a4bb83f54d8f036ad9562a2383ad
Jordi Nebot <jordi.nebot@king.com>
2020-12-12 18:53:08 +0100

    Add solution for 2020 day-12, part-2

--- 2020/day-12/main.py
+++ 2020/day-12/main.py
@@ -1,3 +1,5 @@
+from math import sin, cos, radians
+
 f = open("input", "rt")
 instructions  = f.read().splitlines()
 
@@ -38,3 +40,36 @@ for instruction in instructions:
 			position[1] -= value
 
 print("Part 1: Manhattan distance from starting position is %d" % (abs(position[0]) + abs(position[1])))
+
+
+def rotate(point, angle):
+	s = sin(radians(angle))
+	c = cos(radians(angle))
+
+	return [round(point[0] * c - point[1] * s), round(point[1] * c + point[0] * s)]
+
+position = [0, 0]
+waypoint = [10, 1]
+
+for instruction in instructions:
+	action = instruction[0]
+	value = int(instruction[1:])
+
+	if action == N:
+		waypoint[1] += value
+	elif action == S:
+		waypoint[1] -= value
+	elif action == E:
+		waypoint[0] += value
+	elif action == W:
+		waypoint[0] -= value
+
+	elif action == R:
+		waypoint = rotate(waypoint, -value)
+	elif action == L:
+		waypoint = rotate(waypoint, value)
+	elif action == F:
+		position = [position[0] + value * waypoint[0], position[1] + value * waypoint[1]]
+
+print("Part 2: Manhattan distance from starting position is %d" % (abs(position[0]) + abs(position[1])))
+
