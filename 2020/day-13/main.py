9b6fd6ce13c035d74b5c79a6f38f2b2966c7e32a
Jordi Nebot <jordi.nebot@king.com>
2020-12-13 18:21:16 +0100

    Add brute-force method for 2020 day-13, part-2

--- 2020/day-13/main.py
+++ 2020/day-13/main.py
@@ -1,17 +1,38 @@
 import re
-f = open("input", "rt")
+from functools import reduce
+
+f = open("test", "rt")
 t, buses = f.read().splitlines()
 
 t = int(t)
-buses = [*map(lambda x: int(x), re.sub(",x", "", buses).split(","))]
+ids = [*map(lambda x: int(x), re.sub(",x", "", buses).split(","))]
 
 delta = -1
 found = False
 while not found:
 	delta += 1
-	for bus in buses:
-		if (t + delta) % bus == 0:
+	for id in ids:
+		if (t + delta) % id == 0:
 			found = True
 			break
 
-print("Part 1: Bus ID multiplied by waiting time is %d" % (delta * bus))
+print("Part 1: Bus ID multiplied by waiting time is %d" % (delta * id))
+
+buses = buses.split(",")
+schedule = {}
+for i in range(len(buses)):
+	if buses[i] != "x":
+		schedule[i] = int(buses[i])
+
+t = 0
+delta = -1
+found = False
+
+deltas = [*schedule.keys()]
+while not found:
+	delta +=1
+	departures = [(t + delta + deltas[i]) % id for i, id in enumerate(schedule.values())]
+	if not any(departures):
+		found = True
+
+print("Part 2: Earliest timestamp with offsets matching positions is %d" % (t + delta))
