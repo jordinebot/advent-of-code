0caccdb1d39128358b60c8215b03622a367d719f
Jordi Nebot <jordi.nebot@king.com>
2020-12-14 21:53:23 +0100

    Add solution for 2020 day-14, part-2

--- 2020/day-14/main.py
+++ 2020/day-14/main.py
@@ -5,9 +5,6 @@ MASK = "mask"
 f = open("input", "rt")
 program = f.read().splitlines()
 
-mem = {}
-mask = ''
-
 def to_bin(n):
 	return bin(int(n))[2:]
 
@@ -25,11 +22,56 @@ def set_addr(addr, val):
 	mem[addr] = to_dec(masked)
 
 
+mem = {}
+mask = ''
+
 for ins in program:
 	cmd, val = ins.split(" = ")
 	if cmd == MASK:
 		mask = val
 	else:
 		set_addr(cmd_addr(cmd), val)
+
+print("Part 1: The sum of all values in memory is %d" % sum(mem.values()))
+
+
+def unmask(masked, unmasked):
+	index = masked.index("X") if "X" in masked else None
+	if index == None:
+		unmasked.append(masked)
+	else:
+		a = [*masked]
+		a[index] = "0"
+		b = [*masked]
+		b[index] = "1"
+		a = "".join(a)
+		b = "".join(b)
+		unmask(a, unmasked)
+		unmask(b, unmasked)
+	return unmasked
+
+
+
+
+
+def cmd_addr_v2(cmd):
+	addr = cmd_addr(cmd)
+	bin_addr = [*reversed(to_bin(addr).zfill(len(mask)))]
+	masked = "".join(reversed([m if m == "X" else bin_addr[i] if m == "0" else "1" for i, m in enumerate(reversed(mask))]))
+	return unmask(masked, [])
+
+
+mem = {}
+mask = ''
+
+for ins in program:
+	cmd, val = ins.split(" = ")
+	if cmd == MASK:
+		mask = val
+	else:
+		addrs = cmd_addr_v2(cmd)
+		for addr in addrs:
+			mem[to_dec(addr)] = int(val)
+
 
 print("Part 1: The sum of all values in memory is %d" % sum(mem.values()))
