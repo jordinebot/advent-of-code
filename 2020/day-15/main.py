3485acea4b91807da081feb4c8b0d1933b35ef12
Jordi Nebot <jordi.nebot@king.com>
2020-12-15 19:28:32 +0100

    Add solution for 2020 day-15, part-2

--- 2020/day-15/main.py
+++ 2020/day-15/main.py
@@ -1,14 +1,24 @@
+import time as T
 input = [1,0,16,5,17,4]
 
 def spoken(seq, n):
-	while len(seq) <= n:
-		prev_spoken = seq[-1]
-		if not prev_spoken in seq[:-1]:
-			seq.append(0)
-		else:
-			last_index = len(seq)
-			prev_index = len(seq) - 1 - seq[:-1][::-1].index(prev_spoken)
-			seq.append(last_index - prev_index)
-	return seq[n - 1]
+    mem = { x: i + 1 for i, x in enumerate(seq) }
+    spoken = seq[-1]
+    for t in range(len(seq) + 1, n + 1):
+        last_spoken = spoken
+        spoken = 0 if not last_spoken in mem else t - 1 - mem[last_spoken]
+        mem[last_spoken] = t - 1
+        seq.append(spoken)
+    return seq[n - 1]
 
-print("Part 1: The 2020th number spoken will be %d" % spoken(input, 2020))
+
+t0 = T.time()
+spoken_2020 = spoken(input, 2020)
+t1 = T.time()
+print("Part 1: The 2020th number spoken will be %d (%fms)" % (spoken_2020, (t1 - t0) * 1000))
+
+t0 = T.time()
+spoken_30M = spoken(input, 30000000)
+t1 = T.time()
+print("Part 2: The 30000000th number spoken will be %d (%fms)" % (spoken_30M, (t1 - t0) * 1000))
+
