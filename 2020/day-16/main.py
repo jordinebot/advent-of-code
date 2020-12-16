a4f39280d3e2b049a72392c924b1c3a5b97bb8af
Jordi Nebot <jordi.nebot@king.com>
2020-12-16 17:44:26 +0100

    Add solution for 2020 day-16, part-1

--- 2020/day-16/main.py
+++ 2020/day-16/main.py
@@ -15,3 +15,28 @@ nearby_tickets = []
 for nearby in raw_nearby_tickets.splitlines()[1:]:
 	nearby_tickets.append([int(x) for x in nearby.split(",")])
 
+
+def in_range(value):
+	for r in rules.values():
+		if value in r[0] or value in r[1]:
+			return True
+	return False
+
+
+def is_valid_ticket(ticket):
+	for t in ticket:
+		if in_range(t):
+			return True
+	return False
+
+
+def scanning_error_rate(ticket):
+	out_of_range = [t for t in ticket if not in_range(t)]
+	return sum(out_of_range)
+
+
+nearby_tickets_scanning_error_rate = sum([scanning_error_rate(t) for t in nearby_tickets])
+print("Part 1: The sum of scanning error rate for nearby tickets is %d" % nearby_tickets_scanning_error_rate)
+
+
+# valid_nearby_tickets = [*map(lambda t: is_valid_ticket(t), nearby_tickets)]
