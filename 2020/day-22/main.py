0e819bbf713cf873ac652007956bac9d4f5d4762
Jordi Nebot <jordi.nebot@king.com>
2020-12-23 09:13:36 +0100

    Add solution for 2020 day-22, part-2

--- 2020/day-22/main.py
+++ 2020/day-22/main.py
@@ -1,7 +1,5 @@
+from time import time
 from copy import deepcopy
-
-import sys
-sys.setrecursionlimit(10000)
 
 f = open('./input', 'rt')
 player1, player2 = [p.splitlines()[1:] for p in f.read().strip().split('\n\n')]
@@ -10,71 +8,62 @@ player1 = [*map(lambda x: int(x), player1)]
 player2 = [*map(lambda x: int(x), player2)]
 
 def crab_combat(p1, p2):
-	if len(p1) == 0:
-		return p2
-	if len(p2) == 0:
-		return p1
+    if len(p1) == 0:
+        return p2
+    if len(p2) == 0:
+        return p1
 
-	c1 = p1.pop(0)
-	c2 = p2.pop(0)
+    c1 = p1.pop(0)
+    c2 = p2.pop(0)
 
-	if c1 > c2:
-		p1.extend([c1, c2])
-	else:
-		p2.extend([c2, c1])
-	return crab_combat(p1, p2)
+    if c1 > c2:
+        p1.extend([c1, c2])
+    else:
+        p2.extend([c2, c1])
+
+    return crab_combat(p1, p2)
 
 def score(deck):
-	return sum([(len(deck) - i) * deck[i] for i in range(len(deck))])
+    return sum([(len(deck) - i) * deck[i] for i in range(len(deck))])
 
+t0 = time()
 winner = crab_combat(deepcopy(player1), deepcopy(player2))
-print("\nPart 1: The score of the winner is", score(winner))
+t1 = time()
+print("Part 1: The score of the winner is %d (%fms)" % (score(winner), (t1 - t0) * 100))
+
+def hash_round(p1, p2):
+    return hash((tuple(p1), tuple(p2)))
+
+def iterative_combat(p1, p2, played):
+    while len(p1) > 0 and len(p2) > 0:
+        round_hash = hash_round(p1, p2)
+        if round_hash in played:
+            return (1, p1)
+        else:
+            played.append(round_hash)
+
+        c1 = p1.pop(0)
+        c2 = p2.pop(0)
+
+        if len(p1) >= c1 and len(p2) >= c2:
+            subgame_winner, _winning_deck = iterative_combat(p1[0:c1], p2[0:c2], [])
+            if subgame_winner == 1:
+                p1.extend([c1, c2])
+            else:
+                p2.extend([c2, c1])
+        elif c1 > c2:
+            p1.extend([c1, c2])
+        else:
+            p2.extend([c2, c1])
+
+    if len(p1) == 0:
+        return (2, p2)
+    else:
+        return (1, p1)
 
 
-def recursive_combat(p1, p2, played, game, r):
-	print('\n-- Round %d (Game %d) --' % (r, game))
-	if (p1, p2) in played:
-		print('The winner of game %d is player 1 (repeated hands)' % game)
-		return (1, p1) # Player 1 wins due to repeated hands
+t0 = time()
+player, deck = iterative_combat(deepcopy(player1), deepcopy(player2), [])
+t1 = time()
+print("Part 2: The score of the winner is %d (%fms)" % (score(deck), (t1 - t0) * 100))
 
-	if r == 1:
-		played.append((deepcopy(p1), deepcopy(p2)))
-
-	print('Player 1 deck:', p1)
-	print('Player 2 deck:', p2)
-
-	c1 = p1.pop(0)
-	c2 = p2.pop(0)
-
-	print('Player 1 plays', c1)
-	print('Player 2 plays', c2)
-
-	if len(p1) >= c1 and len(p2) >= c2:
-		print('Playing a sub-game to determine the winner...')
-		subgame_winner, _winning_deck = recursive_combat(deepcopy(p1), deepcopy(p2), [], game + 1, 1)
-		print('\n...anyway, back to game %d' % game)
-		if subgame_winner == 1:
-			p1.extend([c1, c2])
-			print('Player 1 wins round %d of game %d' % (r, game))
-		else:
-			p2.extend([c2, c1])
-			print('Player 2 wins round %d of game %d' % (r, game))
-	elif c1 > c2:
-		print('Player 1 wins round %d of game %d' % (r, game))
-		p1.extend([c1, c2])
-	elif c1 < c2:
-		print('Player 2 wins round %d of game %d' % (r, game))
-		p2.extend([c2, c1])
-
-	if len(p1) == 0:
-		print('The winner of game %d is player 2' % game)
-		return (2, p2)
-	elif len(p2) == 0:
-		print('The winner of game %d is player 1' % game)
-		return (1, p1)
-	else:
-		return recursive_combat(p1, p2, played, game, r + 1)
-
-player, deck = recursive_combat(deepcopy(player1), deepcopy(player2), [], 1, 1)
-print('\nPlayer %d WINS with deck %s' % (player, deck))
-print("\nPart 2: The score of the winner is", score(deck))
