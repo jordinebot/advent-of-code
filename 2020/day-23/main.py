7471f8258ff3f4faa4c57bebd426339e9fc48cb2
Jordi Nebot <jordi.nebot@king.com>
2021-12-01 18:12:42 +0100

    Latest changes from 2020

--- 2020/day-23/main.py
+++ 2020/day-23/main.py
@@ -1,21 +1,18 @@
 from time import time
+from collections import deque
 
-test = [3,8,9,1,2,5,4,6,7]
-input = [7,9,2,8,4,5,1,3,6]
+test = deque([3,8,9,1,2,5,4,6,7])
+input = deque([7,9,2,8,4,5,1,3,6])
 
 def next3(cups, index):
-	t = cups[index + 1: index + 4]
-	if len(t) < 3:
-		t.extend(cups[0:3 - len(t)])
+	t = []
+	index += 1
+	while len(t) < 3:
+		t.append(cups[index % len(cups)])
+		index += 1
 	return t
 
-def rotate(cups):
-	r = cups[1:]
-	r.append(cups[0])
-	return r
-
 def crab_cups(cups, moves):
-	cups = list(cups)
 	p = 0
 	m = min(cups)
 	M = max(cups)
@@ -38,27 +35,27 @@ def crab_cups(cups, moves):
 			cups.insert(di, [*reversed(N)][j])
 
 		while cups[p] != c:
-			cups = rotate(cups)
+			cups.rotate(1)
 
 		p = (p + 1) % len(cups)
 
 	return cups
 
 
-mix = crab_cups(input, 100)
+mix = crab_cups(input.copy(), 100)
 while mix[0] != 1:
-	mix = rotate(mix)
-print('Part 1: The labels on the cups after cup 1 are %s' % ''.join([str(m) for m in mix[1:]]))
+	mix.rotate(1)
+mix.remove(1)
+print('Part 1: The labels on the cups after cup 1 are %s' % ''.join([str(m) for m in list(mix)]))
 
 
 def crab_cups_2(cups, moves):
 	M = max(cups)
-	cups = list(cups)
 	cups.extend([*range(M, 1000001)])
 	return crab_cups(cups, moves)
 
 t0 = time()
-mix = crab_cups_2(test, 10000000)
+mix = crab_cups_2(input.copy(), 10000000)
 t1 = time()
 index_1 = mix.index(1)
 
