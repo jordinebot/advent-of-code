d3df9510276fbb69f71870ac46cb16e03594597b
Jordi Nebot <jordi.nebot@king.com>
2021-12-01 19:13:44 +0100

    Add solution for 2021 day-1, part-2

--- 2021/index.js
+++ 2021/index.js
@@ -1,5 +1,10 @@
 import { readFile } from 'fs';
 
+const countIncrements = (array) =>
+	array.filter((item, index, items) => index > 0 && item > items[index - 1]).length;
+
+const sumArray = (array) => array.reduce((total, num) => total + num, 0);
+
 readFile('./input1', (error, buffer) => {
 	if (error) {
 		throw error;
@@ -10,6 +15,18 @@ readFile('./input1', (error, buffer) => {
 		.split('\n')
 		.map((n) => parseInt(n, 10));
 
-	const increments = data.filter((item, index, items) => index > 0 && item > items[index - 1]).length
+	const increments = countIncrements(data);
 	console.log('increments:', increments);
+
+	const WINDOW_SIZE = 3;
+
+	const sums = data.reduce((result, current, index) => {
+		if (index + WINDOW_SIZE < data.length) {
+			result.push(sumArray(data.slice(index, index + WINDOW_SIZE)));
+		}
+		return result;
+	}, []);
+
+	const sumIncrements = countIncrements(sums);
+	console.log('sumIncrements:', sumIncrements);
 });
