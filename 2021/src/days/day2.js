258340df52c91c1f9caa62db019d3cf9884a012a
Jordi Nebot <jordi.nebot@king.com>
2021-12-02 10:34:44 +0100

    Add solution for 2021 day-2, part-2

--- 2021/src/days/day2.js
+++ 2021/src/days/day2.js
@@ -4,25 +4,42 @@ export const day2 = async () => {
 	const data = await readStrings('src/inputs/input2');
 	const navigation = data
 		.map((instruction) => instruction.split(' '))
-		.map(([direction, amount]) => [direction, parseInt(amount, 10)]);
+		.map(([command, x]) => [command, parseInt(x, 10)]);
 
 	let depth = 0;
 	let hp = 0;
+	let aim = 0;
 
-	navigation.forEach(([direction, amount]) => {
-		switch (direction) {
+	navigation.forEach(([command, x]) => {
+		switch (command) {
 			case 'forward':
-				hp += amount;
+				hp += x;
 				break;
 			case 'down':
-				depth += amount;
+				depth += x;
 				break;
 			case 'up':
-				depth -= amount;
+				depth -= x;
 		}
 	});
 
-	console.log('hp:', hp);
-	console.log('depth:', depth);
-	console.log('result', hp * depth);
+	console.log('part1', hp * depth);
+
+	hp = depth = 0;
+	navigation.forEach(([command, x]) => {
+		switch (command) {
+			case 'forward':
+				hp += x;
+				depth += aim * x;
+				break;
+			case 'down':
+				aim += x;
+				break;
+			case 'up':
+				aim -= x;
+		}
+	});
+
+	console.log('part2', hp * depth);
+
 };
