e157e75ba7afc632eea107eb5e723e5dc0b66e25
Jordi Nebot <jordi.nebot@king.com>
2021-12-06 13:27:21 +0100

    Add solution for 2021 day-6, part-2

--- 2021/src/days/day06.js
+++ 2021/src/days/day06.js
@@ -1,10 +1,9 @@
 import { readNumbers } from '../common/helpers';
 
-const ROUNDS = 80;
-
 export const day6 = async () => {
 	const data = await readNumbers('src/inputs/input06', ',');
 
+	let ROUNDS = 80;
 	let lanterfish = [...data];
 	for (let i = 0; i < ROUNDS; i++) {
 		let born = 0;
@@ -22,4 +21,27 @@ export const day6 = async () => {
 	console.log('>>> Day 6');
 	console.log('\tpart1:', lanterfish.length);
 
+	let school = {};
+	data.forEach((fish) => (school[fish] = school[fish] !== undefined ? school[fish] + 1 : 1));
+
+	const generation = (currentGeneration) => {
+		const nextGeneration = {};
+		Object.entries(currentGeneration).forEach(([cicle, quantity]) => {
+			if (cicle == 0) {
+				nextGeneration['8'] = quantity;
+				nextGeneration['6'] =  quantity;
+			} else {
+				nextGeneration[`${cicle - 1}`] = (nextGeneration[`${cicle - 1}`] || 0) + quantity;
+			}
+		});
+		return nextGeneration;
+	};
+
+	ROUNDS = 256;
+	for (let i = 0; i < ROUNDS; i++) {
+		school = generation(school);
+	}
+
+	lanterfish = Object.values(school).reduce((total, quantity) => total + quantity, 0);
+	console.log('\tpart2:', lanterfish);
 };
