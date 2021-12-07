30690bda81bdf5dd81766946469b13af203f2e83
Jordi Nebot <jordi.nebot@king.com>
2021-12-07 12:22:00 +0100

    Add solution for 2021 day-7, part-2

--- 2021/src/days/day07.js
+++ 2021/src/days/day07.js
@@ -2,20 +2,29 @@ import { readNumbers, median, average, range } from '../common/helpers';
 
 export const day7 = async () => {
 	// const data = await readNumbers('src/inputs/sample07', ',');
-	const data = await readNumbers('src/inputs/input07', ',')
+	const data = await readNumbers('src/inputs/input07', ',');
 
 	const m = median(data);
 	const a = Math.ceil(average(data));
-	const explore = range(Math.min(a, m), Math.max(a, m));
 
-	const fuel = {};
-	let lowest = Infinity;
-	explore.forEach((position) => {
-		fuel[position] = 0;
-		data.forEach((crab) => (fuel[position] += Math.abs(crab - position)));
-		lowest = fuel[position] < lowest ? fuel[position] : lowest;
-	});
+	let fuel = {};
+	const position = median(data);
+	fuel[position] = 0;
+	data.forEach((crab) => (fuel[position] += Math.abs(crab - position)));
 
 	console.log('>>> Day 7');
-	console.log('\tpart1:', lowest);
+	console.log('\tpart1:', fuel[position]);
+
+	fuel = {};
+	let lowest = Infinity;
+	const explore = range(Math.min(a, m), Math.max(a, m));
+	explore.forEach((position) => {
+		fuel[position] = 0;
+		data.forEach((crab) => {
+			const jumps = Math.abs(crab - position);
+			fuel[position] += (jumps * (jumps + 1)) / 2;
+		});
+		lowest = fuel[position] < lowest ? fuel[position] : lowest;
+	});
+	console.log('\tpart2:', lowest);
 };
