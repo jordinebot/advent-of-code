5a4e2570310cb66fc7092a8f1ef18af57d25a142
Jordi Nebot <jordi.nebot@king.com>
2021-12-12 13:28:13 +0100

    Add solution for 2021 day-12, part-2

--- 2021/src/days/day12.js
+++ 2021/src/days/day12.js
@@ -6,10 +6,12 @@ function buildMap(input) {
 	const map = {};
 	input.forEach((segment) => {
 		const [from, to] = segment.split('-');
-		if (map[from] === undefined) {
-			map[from] = [to];
-		} else {
-			map[from].push(to);
+		if (from !== 'end' && to !== 'start') {
+			if (map[from] === undefined) {
+				map[from] = [to];
+			} else {
+				map[from].push(to);
+			}
 		}
 		if (to !== 'end' && from !== 'start') {
 			if (map[to] === undefined) {
@@ -22,41 +24,73 @@ function buildMap(input) {
 	return map;
 }
 
-function isSmallCave(cave) {
-	return cave.charCodeAt(0) > LOWERCASE_A_ASCII;
+function isBigCave(cave) {
+	return cave.charCodeAt(0) < LOWERCASE_A_ASCII;
 }
 
 function isFinished(path) {
 	return path[path.length - 1] === 'end';
 }
 
-function explore(path, map) {
+function allowOnce(option, path) {
+	return !path.includes(option);
+}
+
+function allowTwice(option, path) {
+	const smallCaves = path.filter((cave) => !isBigCave(cave));
+	const visits = smallCaves.reduce((visits, cave) => {
+		visits[cave] = visits[cave] !== undefined ? visits[cave] + 1 : 1;
+		return visits;
+	}, {});
+	return (
+		visits[option] === undefined ||
+		(visits[option] === 1 && Object.values(visits).filter((visit) => visit > 1).length === 0)
+	);
+}
+
+function move1(path, map) {
 	const position = path[path.length - 1];
 	const options = map[position];
 	return options
-		.filter((option) => !isSmallCave(option) || !path.includes(option))
+		.filter((option) => isBigCave(option) || allowOnce(option, path))
 		.map((option) => [...path, option]);
 }
 
-function part1(map) {
+function move2(path, map) {
+	const position = path[path.length - 1];
+	const options = map[position];
+	return options
+		.filter((option) => option !== 'start' && isBigCave(option) || allowTwice(option, path))
+		.map((option) => [...path, option]);
+}
+
+function explore(map, goExploring) {
 	let exploring = [['start']];
 	let completed = [];
 	do {
 		let explored = [];
 		exploring.forEach((path) => {
-			explored = [...explored, ...explore(path, map)];
+			explored = [...explored, ...goExploring(path, map)];
 		});
 		exploring = explored.filter((path) => !isFinished(path));
 		completed = [...completed, ...explored.filter((path) => isFinished(path))];
 	} while (exploring.length);
 	return completed.length;
 }
-function part2(input) {}
+
+function part1(caves) {
+	return explore(caves, move1);
+}
+
+function part2(caves) {
+	return explore(caves, move2);
+}
 
 export async function day12() {
 	const data = await readStrings('data/input12');
+	const caves = buildMap(data);
 
 	console.log('\nDay 12 >>>');
-	console.log('    Part 1:', part1(buildMap(data)));
-	console.log('    Part 2:', part2(data));
+	console.log('    Part 1:', part1(caves));
+	console.log('    Part 2:', part2(caves));
 }
